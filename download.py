17a4d29554905fc9b62f95af00470e3884eee3e7
duyminh1998 <minhhua12345@gmail.com>
2020-01-29 17:00:45 -0800

    download code

--- download.py
+++ download.py
@@ -1,19 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import random
 
-df = pd.DataFrame(columns = ["realTitle", "criticsConcensus", "tomatoMeter", "tomatoCount", 
+df = pd.DataFrame(columns = ["dataIndex", "realTitle", "criticsConcensus", "tomatoMeter", "tomatoCount", 
             "audienceScore", "audienceCount", "realSynopsis", "rating", "genreString", 
             "directedBy", "studio", "runTime", "cast"])
 
 data = pd.read_csv("movies_metadata.csv", error_bad_lines=False, dtype='unicode')
 
+# names = random.sample(list(data["original_title"]), 1000)
+
+# for Trung: range(15001, 30000)
+# for Quang: range(30001, len(data) - 1)
+
 j = 0
 err_list = []
-for i in range(0, 1000): # CHANGE THIS
+for i in range(100, 150):
     #print("https://www.rottentomatoes.com/m/" + data.at[i, "original_title"].lower().replace(" ", "_"))
     try:
         movie_data = printData("https://www.rottentomatoes.com/m/" + data.at[i, "original_title"].lower().replace(" ", "_"))
+        #movie_data = printData("https://www.rottentomatoes.com/m/" + names[i].lower().replace(" ", "_"))
         df.at[j, "realTitle"] = movie_data[0]
         df.at[j, "criticsConcensus"] = movie_data[1]
         df.at[j, "tomatoMeter"] = movie_data[2]
@@ -27,13 +34,14 @@ for i in range(0, 1000): # CHANGE THIS
         df.at[j, "studio"] = movie_data[10]
         df.at[j, "runTime"] = movie_data[11]
         df.at[j, "cast"] = movie_data[12]
+        df.at[j, "dataIndex"] = i
         j += 1
         # print(printData("https://www.rottentomatoes.com/m/" + data.at[i, "original_title"].lower().replace(" ", "_")))
     except:
         err_list.append(data.at[i, "original_title"])
         pass
 
-with open("errors.txt", "w") as output:
-    output.write(str(err_list))
+with open("errors" + str(df.at[len(df) - 1, "dataIndex"]) + ".txt", "w") as output:
+    output.write(str(str(err_list).encode('utf8')))
 
-df.to_csv("data.csv")
+df.to_csv("data-" + str(df.at[len(df) - 1, "dataIndex"]) + ".csv")
